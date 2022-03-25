4e3b3df23af9d3ad79ce751fc97abe75c1cf9ea8
Ivanov Andrey <this.andrey@gmail.com>
2022-03-25 10:38:39 +0200

    add app template, .gitignor(s), cleanup

---
+++ app/lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({
+    Key? key,
+  }) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) => const MaterialApp();
+}
