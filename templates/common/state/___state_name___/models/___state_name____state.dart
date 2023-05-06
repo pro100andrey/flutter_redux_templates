32ced94f26ea062399c44d40c4add4973c5637d4
Andrey Ivanov <this.andrey@gmail.com>
2023-05-06 12:12:34 +0300

    added state templeates

---
+++ templates/common/state/___state_name___/models/___state_name____state.dart
@@ -0,0 +1,10 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part '___state_name____state.freezed.dart';
+
+@freezed
+class ___StateName___State with _$___StateName___State {
+  const factory ___StateName___State({
+    String? value,
+  }) = ____StateName___State;
+}
---
+++ templates/common/state_search/___state_name___/___state_name____selectors.dart
@@ -0,0 +1,11 @@
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
+
+import '../app_state.dart';
+
+/// Returns search query string
+String? select___StateName___Query(AppState state) =>
+    state.___stateName___.query;
+
+/// Returns search results view
+IList<int> select___StateName___View(AppState state) =>
+    state.___stateName___.view;
