e57f327f1cf7f8398a9f5851381284014af80353
Andrey Ivanov <this.andrey@gmail.com>
2024-05-11 12:22:47 +0300

    cleanup

---
+++ business/lib/redux/registration/registration_state.freezed.dart
@@ -0,0 +1,172 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'registration_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$RegistrationState {
+  String? get email => throw _privateConstructorUsedError;
+  String? get password => throw _privateConstructorUsedError;
+  String? get confirmPassword => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $RegistrationStateCopyWith<RegistrationState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $RegistrationStateCopyWith<$Res> {
+  factory $RegistrationStateCopyWith(
+          RegistrationState value, $Res Function(RegistrationState) then) =
+      _$RegistrationStateCopyWithImpl<$Res, RegistrationState>;
+  @useResult
+  $Res call({String? email, String? password, String? confirmPassword});
+}
+
+/// @nodoc
+class _$RegistrationStateCopyWithImpl<$Res, $Val extends RegistrationState>
+    implements $RegistrationStateCopyWith<$Res> {
+  _$RegistrationStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? email = freezed,
+    Object? password = freezed,
+    Object? confirmPassword = freezed,
+  }) {
+    return _then(_value.copyWith(
+      email: freezed == email
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String?,
+      password: freezed == password
+          ? _value.password
+          : password // ignore: cast_nullable_to_non_nullable
+              as String?,
+      confirmPassword: freezed == confirmPassword
+          ? _value.confirmPassword
+          : confirmPassword // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$RegistrationStateImplCopyWith<$Res>
+    implements $RegistrationStateCopyWith<$Res> {
+  factory _$$RegistrationStateImplCopyWith(_$RegistrationStateImpl value,
+          $Res Function(_$RegistrationStateImpl) then) =
+      __$$RegistrationStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String? email, String? password, String? confirmPassword});
+}
+
+/// @nodoc
+class __$$RegistrationStateImplCopyWithImpl<$Res>
+    extends _$RegistrationStateCopyWithImpl<$Res, _$RegistrationStateImpl>
+    implements _$$RegistrationStateImplCopyWith<$Res> {
+  __$$RegistrationStateImplCopyWithImpl(_$RegistrationStateImpl _value,
+      $Res Function(_$RegistrationStateImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? email = freezed,
+    Object? password = freezed,
+    Object? confirmPassword = freezed,
+  }) {
+    return _then(_$RegistrationStateImpl(
+      email: freezed == email
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String?,
+      password: freezed == password
+          ? _value.password
+          : password // ignore: cast_nullable_to_non_nullable
+              as String?,
+      confirmPassword: freezed == confirmPassword
+          ? _value.confirmPassword
+          : confirmPassword // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$RegistrationStateImpl implements _RegistrationState {
+  const _$RegistrationStateImpl(
+      {this.email, this.password, this.confirmPassword});
+
+  @override
+  final String? email;
+  @override
+  final String? password;
+  @override
+  final String? confirmPassword;
+
+  @override
+  String toString() {
+    return 'RegistrationState(email: $email, password: $password, confirmPassword: $confirmPassword)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$RegistrationStateImpl &&
+            (identical(other.email, email) || other.email == email) &&
+            (identical(other.password, password) ||
+                other.password == password) &&
+            (identical(other.confirmPassword, confirmPassword) ||
+                other.confirmPassword == confirmPassword));
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, email, password, confirmPassword);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$RegistrationStateImplCopyWith<_$RegistrationStateImpl> get copyWith =>
+      __$$RegistrationStateImplCopyWithImpl<_$RegistrationStateImpl>(
+          this, _$identity);
+}
+
+abstract class _RegistrationState implements RegistrationState {
+  const factory _RegistrationState(
+      {final String? email,
+      final String? password,
+      final String? confirmPassword}) = _$RegistrationStateImpl;
+
+  @override
+  String? get email;
+  @override
+  String? get password;
+  @override
+  String? get confirmPassword;
+  @override
+  @JsonKey(ignore: true)
+  _$$RegistrationStateImplCopyWith<_$RegistrationStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
